32114e1c45262bdcf7ca25017c93062d63690439
Blxee <aminedev57@gmail.com>
2023-10-05 23:33:30 +0100

    Added: 101-setup_web_static.pp task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,20 +1,28 @@
 # 5. Puppet for setup
-file { '/data/web_static/shared/':
+file { 'shared':
+  path    =>  '/data/web_static/shared/',
   ensure  => directory,
   recurse => true,
 }
-file { '/data/web_static/releases/test/':
+file { 'test':
+  path    =>  '/data/web_static/releases/test/',
   ensure  => directory,
   recurse => true,
+  require => File['shared'],
 }
-file { '/data/web_static/releases/test/index.html':
+file { 'index':
+  path    =>  '/data/web_static/releases/test/index.html',
   ensure  => file,
   recurse => true,
   content => '<html>\n<body>\n<h1>Hello World!</h1>\n</body>\n</html>',
+  require => File['test'],
 }
-exec { 'ln -sf /data/web_static/releases/test/ /data/web_static/current':
+exec { 'ln':
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
   provider => shell,
+  require  => File['index'],
 }
 exec { 'chown -R ubuntu:ubuntu /data/':
   provider => shell,
+  require  => Exec['ln'],
 }
