e9e0d2691eece3b9d6c043d5930613f8ba509339
Blxee <aminedev57@gmail.com>
2023-10-05 11:56:52 +0100

    Added: 0-setup_web_static.sh task

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# 0. Prepare your web servers
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo -n "\
+<html>
+  <body>
+    <h1>Hello World!</h1>
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
