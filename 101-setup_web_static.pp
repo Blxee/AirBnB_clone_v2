9a2949aa082c4ee9640d226570b8c3fc227290a9
Blxee <aminedev57@gmail.com>
2023-10-08 15:08:27 +0100

    Added: 101-setup_web_static.pp task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,14 +1,14 @@
 # 5. Puppet for setup
 file { 'shared':
   path    =>  '/data/web_static/shared/',
-  ensure  => directory,
+  ensure  => 'directory',
   recurse => true,
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
 file { 'test':
   path    =>  '/data/web_static/releases/test/',
-  ensure  => directory,
+  ensure  => 'directory',
   recurse => true,
   owner   => 'ubuntu',
   group   => 'ubuntu',
@@ -16,7 +16,7 @@ file { 'test':
 }
 file { 'index':
   path    =>  '/data/web_static/releases/test/index.html',
-  ensure  => file,
+  ensure  => 'file',
   recurse => true,
   content => '<html>\n<body>\n<h1>Hello World!</h1>\n</body>\n</html>',
   owner   => 'ubuntu',
