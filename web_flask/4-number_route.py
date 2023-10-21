cc73e9fcd334f17c2d853be1f0b9cebcc56f5705
Blxee <aminedev57@gmail.com>
2023-10-21 12:29:40 +0100

    Added: task 4 of web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" 4. Is it a number? """
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ This function Handles the '/' route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ This function Handles the '/hbnb' route """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ This function Handles the '/c/<text>' route """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text=None):
+    """ This function Handles the '/python/<text>' route """
+    if text is None:
+        text = 'is cool'
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ This function Handles the '/number/<n>' route """
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
